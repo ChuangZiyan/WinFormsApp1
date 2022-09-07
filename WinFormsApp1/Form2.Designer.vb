e24a57a2eb22ecd03944d63a2a666fbb6654f210
ChuangZiyan <ziyan.zhuang@h3platform.com>
2022-09-07 18:13:21 +0800

    add error log filter and search feature

--- WinFormsApp1/Form2.Designer.vb
+++ WinFormsApp1/Form2.Designer.vb
@@ -25,6 +25,10 @@ Partial Class Form2
         Me.test_success_log_btn = New System.Windows.Forms.Button()
         Me.test_fail_log_btn = New System.Windows.Forms.Button()
         Me.eventlog_ListView = New System.Windows.Forms.ListView()
+        Me.show_all_log_btn = New System.Windows.Forms.Button()
+        Me.show_err_log_btn = New System.Windows.Forms.Button()
+        Me.eventlog_search_TextBox = New System.Windows.Forms.TextBox()
+        Me.eventlog_search_btn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'test_success_log_btn
@@ -47,26 +51,72 @@ Partial Class Form2
         '
         'eventlog_ListView
         '
-        Me.eventlog_ListView.Location = New System.Drawing.Point(12, 12)
+        Me.eventlog_ListView.Location = New System.Drawing.Point(12, 47)
         Me.eventlog_ListView.Name = "eventlog_ListView"
-        Me.eventlog_ListView.Size = New System.Drawing.Size(999, 503)
+        Me.eventlog_ListView.Size = New System.Drawing.Size(999, 468)
         Me.eventlog_ListView.TabIndex = 5
         Me.eventlog_ListView.UseCompatibleStateImageBehavior = False
+        '
+        'show_all_log_btn
+        '
+        Me.show_all_log_btn.Font = New System.Drawing.Font("Microsoft JhengHei UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.show_all_log_btn.Location = New System.Drawing.Point(12, 12)
+        Me.show_all_log_btn.Name = "show_all_log_btn"
+        Me.show_all_log_btn.Size = New System.Drawing.Size(94, 29)
+        Me.show_all_log_btn.TabIndex = 6
+        Me.show_all_log_btn.Text = "All"
+        Me.show_all_log_btn.UseVisualStyleBackColor = True
+        '
+        'show_err_log_btn
+        '
+        Me.show_err_log_btn.Font = New System.Drawing.Font("Microsoft JhengHei UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.show_err_log_btn.ForeColor = System.Drawing.Color.Red
+        Me.show_err_log_btn.Location = New System.Drawing.Point(112, 12)
+        Me.show_err_log_btn.Name = "show_err_log_btn"
+        Me.show_err_log_btn.Size = New System.Drawing.Size(94, 29)
+        Me.show_err_log_btn.TabIndex = 7
+        Me.show_err_log_btn.Text = "Error"
+        Me.show_err_log_btn.UseVisualStyleBackColor = True
+        '
+        'eventlog_search_TextBox
+        '
+        Me.eventlog_search_TextBox.Location = New System.Drawing.Point(710, 14)
+        Me.eventlog_search_TextBox.Name = "eventlog_search_TextBox"
+        Me.eventlog_search_TextBox.Size = New System.Drawing.Size(201, 27)
+        Me.eventlog_search_TextBox.TabIndex = 8
+        '
+        'eventlog_search_btn
+        '
+        Me.eventlog_search_btn.Location = New System.Drawing.Point(917, 12)
+        Me.eventlog_search_btn.Name = "eventlog_search_btn"
+        Me.eventlog_search_btn.Size = New System.Drawing.Size(94, 29)
+        Me.eventlog_search_btn.TabIndex = 9
+        Me.eventlog_search_btn.Text = "Search"
+        Me.eventlog_search_btn.UseVisualStyleBackColor = True
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1023, 588)
+        Me.Controls.Add(Me.eventlog_search_btn)
+        Me.Controls.Add(Me.eventlog_search_TextBox)
+        Me.Controls.Add(Me.show_err_log_btn)
+        Me.Controls.Add(Me.show_all_log_btn)
         Me.Controls.Add(Me.eventlog_ListView)
         Me.Controls.Add(Me.test_fail_log_btn)
         Me.Controls.Add(Me.test_success_log_btn)
         Me.Name = "Form2"
         Me.Text = "Selenium Log"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents test_success_log_btn As Button
     Friend WithEvents test_fail_log_btn As Button
     Friend WithEvents eventlog_ListView As ListView
+    Friend WithEvents show_all_log_btn As Button
+    Friend WithEvents show_err_log_btn As Button
+    Friend WithEvents eventlog_search_TextBox As TextBox
+    Friend WithEvents eventlog_search_btn As Button
 End Class
--- WinFormsApp1/Form2.vb
+++ WinFormsApp1/Form2.vb
@@ -1,9 +1,6 @@
 ﻿Public Class Form2
     Dim log_path = My.Computer.FileSystem.CurrentDirectory + "\logs"
 
-    Private Sub form2_clear_log_btn_Click_1(sender As Object, e As EventArgs) Handles form2_clear_log_btn.Click
-        form2_logs_RichTextBox.Text = ""
-    End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles Me.Load
         eventlog_ListView.View = View.Details
@@ -33,12 +30,11 @@
 
 
     Public Sub Update_eventlog_listview()
-
+        eventlog_ListView.Items.Clear()
         Dim log_lines = IO.File.ReadAllLines(log_path + "\selenium_log_temp.txt").Reverse()
         Dim curr_row As Integer = 0
-        Dim index As Integer = IO.File.ReadAllLines(log_path + "\selenium_log_temp.txt").Count
+        Dim index As Integer = IO.File.ReadAllLines(log_path + "\selenium_log_temp.txt").Length
         For Each line In log_lines
-            Debug.WriteLine(line)
             Dim splittedLine() As String = line.Split(",")
             eventlog_ListView.Items.Add(index.ToString)
             For Each log In splittedLine
@@ -49,6 +45,8 @@
         Next
 
     End Sub
+
+
 
     Public Sub EventlogListview_AddNewItem(content)
         Dim curr_row = eventlog_ListView.Items.Count + 1
@@ -70,18 +68,39 @@
         Form1.Write_err_log(content)
     End Sub
 
-    Private Sub form2_save_log_btn_Click(sender As Object, e As EventArgs) Handles form2_save_log_btn.Click
-        'MsgBox(My.Computer.FileSystem.CurrentDirectory)
-        Dim thisDate As String = Date.Today.ToString("dd-MM-yyyy")
-        'Exit Sub
-        Dim log_path = My.Computer.FileSystem.CurrentDirectory + "\logs\" + thisDate
-        If Not System.IO.Directory.Exists(log_path) Then
-            System.IO.Directory.CreateDirectory(log_path)
-        End If
-        Dim log_file As System.IO.StreamWriter
-        log_file = My.Computer.FileSystem.OpenTextFileWriter(log_path + "\selenium.log", True)
-        log_file.WriteLine(form2_logs_RichTextBox.Text)
-        log_file.Close()
-        MsgBox("Save log file to " + log_path)
+    Private Sub Show_err_log_btn_Click(sender As Object, e As EventArgs) Handles show_err_log_btn.Click
+        eventlog_ListView.Items.Clear()
+        Filter_and_update_eventlog("Error", 2)
+    End Sub
+
+    Private Sub show_all_log_btn_Click(sender As Object, e As EventArgs) Handles show_all_log_btn.Click
+        Update_eventlog_listview()
+    End Sub
+
+    Private Sub eventlog_search_btn_Click(sender As Object, e As EventArgs) Handles eventlog_search_btn.Click
+        Dim search_str As String = eventlog_search_TextBox.Text
+        Filter_and_update_eventlog(search_str, 3)
+
+    End Sub
+
+
+    Private Sub Filter_and_update_eventlog(str As String, item As Integer) ' 2:level   3:content
+        eventlog_ListView.Items.Clear()
+        Dim log_lines = IO.File.ReadAllLines(log_path + "\selenium_log_temp.txt").Reverse()
+        Dim curr_row As Integer = 0
+        Dim index As Integer = IO.File.ReadAllLines(log_path + "\selenium_log_temp.txt").Length
+        For Each line In log_lines
+            Dim splittedLine() As String = line.Split(",")
+            If Not splittedLine(item).Contains(str) Then
+                Continue For
+            End If
+            eventlog_ListView.Items.Add(index.ToString)
+            For Each log In splittedLine
+                eventlog_ListView.Items(curr_row).SubItems.Add(log)
+            Next
+            curr_row += 1
+            index -= 1
+        Next
+
     End Sub
 End Class
